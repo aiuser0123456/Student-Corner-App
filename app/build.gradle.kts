6c770d5664b16cfe1b481dd37fe6ecf3cbbca600
aiuser0123456 <aiuser0123456@gmail.com>
2026-06-09 09:55:15 +0530

    ask version change from 35 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.studentcorner"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.studentcorner"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
 
